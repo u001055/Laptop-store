564f0d2e43ee077c24272511d497edb16e95045f
Oleg <52830599+u001055@users.noreply.github.com>
2020-04-25 19:00:02 +0300

    pathName data

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    console.log('Someone did access the server!');
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if (pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end('This is the PRODUCTS page');
+    }
+
+    else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+        res.end('This is the LAPTOP page #' + id);
+    } 
+
+    else {
+        res.writeHead(404, { 'Content-type': 'text/html'});
+        res.end('URL not found');
+    }
+
+
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
